c7ab578b103821b50464029612bfb4f4444b47f9
ThnhT4m <thanhtamdz.2210@gmail.com>
2026-03-23 15:54:45 +0700

    new sol

--- CheckIfTwoStringArraysareEquivalent.c
+++ CheckIfTwoStringArraysareEquivalent.c
@@ -1,3 +1,10 @@
+ int myStrcmp(char *s1 , char *s2){
+    while(*s1 && *s2 && *s1==*s2){
+        ++s1;
+        ++s2;
+    }
+    return *s1 - *s2;
+}
  bool arrayStringsAreEqual(char** word1, int word1Size, char** word2, int word2Size) {
     char str1[10001] = "";
     char str2[10001] = "";
@@ -7,7 +14,7 @@
         for(int i = 0 ; i < word2Size; i++){
         strcat(str2,word2[i]);
     }
-    if(strcasecmp(str1,str2) == 0){
+    if(myStrcmp(str1,str2) == 0){
         return true;
     }else{
         return false;
